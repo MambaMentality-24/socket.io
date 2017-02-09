5b45be133b5a3ebb9b22fdc3709a1b69f39032d0
屁丶屁 <kongge_3@163.com>
2017-02-09 18:44:33 +0800

    Update model.js

--- model.js
+++ model.js
@@ -1,5 +1,5 @@
 let mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1/chat');
+mongoose.connect('mongodb://localhost/chat');
 let MessageSchema = new mongoose.Schema({
     username: String,
     content: String,
